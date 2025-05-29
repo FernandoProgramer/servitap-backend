60a4ad1c025715aa6c43190bab69f2c569327ac2
FernandoAlfonso <alfonso07amaya@gmail.com>
2025-05-29 15:14:47 -0500

    Implementación, creación de carpetas-entidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { RolesModule } from './roles/roles.module';
+import { CategoriesModule } from './categories/categories.module';
+import { DishesModule } from './dishes/dishes.module';
+import { TablesModule } from './tables/tables.module';
+import { OrdersModule } from './orders/orders.module';
+import { BillsModule } from './bills/bills.module';
 
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, RestaurantsModule, RolesModule, CategoriesModule, DishesModule, TablesModule, OrdersModule, BillsModule],
   controllers: [],
   providers: [],
 })
